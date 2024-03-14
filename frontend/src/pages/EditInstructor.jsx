36003c595ca001fdba13c6adbc39f8af69c84c25
Preeth Kanamangala <preethkanamangala@Preeths-Air.lan>
2024-03-14 13:57:08 -0500

    adding view, add, edit, and delete for instructor model

--- frontend/src/pages/EditInstructor.jsx
+++ frontend/src/pages/EditInstructor.jsx
@@ -1,8 +1,94 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import BackButton from '../../components/BackButton';
+import Spinner from '../../components/Spinner';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
 
 const EditInstructor = () => {
+  const [name, setName] = useState('');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const {id} = useParams();
+  const { enqueueSnackbar } = useSnackbar();
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get(`http://localhost:5555/instructors/${id}`)
+    .then((response) => {
+        setName(response.data.name);
+        setTitle(response.data.title)
+        setDescription(response.data.description)
+        setLoading(false);
+      }).catch((error) => {
+        setLoading(false);
+        alert('An error happened. Please Chack console');
+        console.log(error);
+      });
+  }, [])
+  
+  const handleEditInstructor = () => {
+    const data = {
+      name,
+      title,
+      description,
+    };
+    setLoading(true);
+    axios
+      .put(`http://localhost:5555/instructors/${id}`, data)
+      .then(() => {
+        setLoading(false);
+        enqueueSnackbar('Instructor edited successfully', { variant: 'success' });
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        // alert('An error happened. Please Chack console');
+        enqueueSnackbar('Error', { variant: 'error' });
+        console.log(error);
+      });
+  };
+
   return (
-    <div>editInstructor</div>
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Edit instructor</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>name</label>
+          <input
+            type='text'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>title</label>
+          <input
+            type='text'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2  w-full '
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>description</label>
+          <input
+            type='text'
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2  w-full '
+          />
+        </div>
+        <button className='p-2 bg-sky-300 m-8' onClick={handleEditInstructor}>
+          Save
+        </button>
+      </div>
+    </div>
   )
 }
 
